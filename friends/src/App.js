846bbc28f89b385d95cf2c4008da897afb269bfb
Daniel Gamboa <danielgamboa10@gmail.com>
2021-02-23 17:23:31 -0600

    Add private route for add friend form

--- friends/src/App.js
+++ friends/src/App.js
@@ -5,6 +5,7 @@ import { Route, Switch } from 'react-router-dom';
 import Login from './components/Login';
 import Friends from './components/Friends';
 import PrivateRoute from './components/PrivateRoute';
+import AddFriend from './components/AddFriend';
 
 // Styles
 import './App.css';
@@ -14,6 +15,7 @@ function App() {
     <div className="App">
       <Switch>
         <PrivateRoute exact path="/friends" component={Friends}/>
+        <PrivateRoute exact path="/add-friend" component={AddFriend}/>
         <Route path="/login" component={Login}/>
         <Route component={Login}/>
       </Switch>

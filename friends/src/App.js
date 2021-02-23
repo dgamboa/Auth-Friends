130b9d37e81751d53b2f5ed4be4730cdffbd130d
Daniel Gamboa <danielgamboa10@gmail.com>
2021-02-23 17:53:25 -0600

    Add nav bar to have access to Friends and Add Friend

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,4 +1,5 @@
 // Modules
+import React, { useEffect, useState } from "react";
 import { Route, Switch } from 'react-router-dom';
 
 // Components
@@ -6,13 +7,25 @@ import Login from './components/Login';
 import Friends from './components/Friends';
 import PrivateRoute from './components/PrivateRoute';
 import AddFriend from './components/AddFriend';
+import NavBar from './components/NavBar';
 
 // Styles
 import './App.css';
 
 function App() {
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  useEffect(() => {
+    if (localStorage.getItem("token")) {
+      setLoggedIn(true);
+    } else {
+      setLoggedIn(false);
+    }
+  })
+
   return (
     <div className="App">
+      <NavBar loggedIn={loggedIn}/>
       <Switch>
         <PrivateRoute exact path="/friends" component={Friends}/>
         <PrivateRoute exact path="/add-friend" component={AddFriend}/>
---
+++ friends/src/components/NavBar.js
@@ -0,0 +1,56 @@
+import React from "react";
+import Nav from 'react-bootstrap/Nav';
+import { useHistory } from 'react-router-dom';
+
+export default function NavBar({ loggedIn }) {
+  const history = useHistory();
+
+  const goHome = () => {
+    history.push("/");
+  };
+
+  const login = () => {
+    history.push("/login");
+  };
+
+  const logout = () => {
+    localStorage.removeItem("token");
+    history.push("/");
+  };
+  
+  const friends = () => {
+    history.push("/friends");
+  }
+
+  const addFriend = () => {
+    history.push("/add-friend");
+  }
+
+  return (
+    <Nav defaultActiveKey="/home" as="ul">
+      <Nav.Item as="li">
+        <Nav.Link onClick={goHome}>Home</Nav.Link>
+      </Nav.Item>
+      {
+        loggedIn
+          ? <Nav.Item as="li">
+              <Nav.Link eventKey="link-1" onClick={logout}>Logout</Nav.Link>
+            </Nav.Item>
+          : <Nav.Item as="li">
+              <Nav.Link eventKey="link-2" onClick={login}>Login</Nav.Link>
+            </Nav.Item>
+      }
+      {
+        loggedIn &&
+        <>
+          <Nav.Item as="li">
+            <Nav.Link eventKey="link-1" onClick={friends}>Friends</Nav.Link>
+          </Nav.Item>
+          <Nav.Item as="li">
+            <Nav.Link eventKey="link-1" onClick={addFriend}>Add Friend</Nav.Link>
+          </Nav.Item>
+        </>
+      }
+    </Nav>
+  )
+}

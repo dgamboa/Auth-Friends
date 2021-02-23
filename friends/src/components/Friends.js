fa786b87edb238ad495fd6b5a2846c540f3fc132
Daniel Gamboa <danielgamboa10@gmail.com>
2021-02-23 16:49:36 -0600

    Write Friends component to fetch data and display Friend component

--- friends/src/components/Friends.js
+++ friends/src/components/Friends.js
@@ -1,9 +1,34 @@
-import React from 'react'
+// Modules
+import React, { useState, useEffect } from 'react'
+
+// Utils
+import { axiosWithAuth } from "../utils/axiosWithAuth";
 
 export default function Friends() {
+  const [friends, setFriends] = useState([]);
+
+  useEffect(() => {
+    getFriends();
+  }, [])
+
+  const getFriends = () => {
+    axiosWithAuth()
+      .get("/api/friends")
+      .then(res => {
+        setFriends(res.data);
+      })
+      .catch(err => {
+        console.log(err.response.data.error);
+      })
+  }
+  
   return (
     <div>
-      Hello      
+      {
+        friends.map(friend => {
+          <Friend friend={friend} />
+        })
+      }
     </div>
   )
 }
